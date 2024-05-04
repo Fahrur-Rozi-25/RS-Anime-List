8b376215714f777615a1c71216d4c82dd6a04361
FahrurRozi <akungamesaya123456@gmail.com>
2024-05-04 20:59:10 +0700

    adjust video

--- src/components/Utilities/VideoPlayer.tsx
+++ src/components/Utilities/VideoPlayer.tsx
@@ -2,42 +2,41 @@
 import YouTube from "react-youtube";
 
 const VideoPlayer = ({ youTubeId }: any) => {
-
   const option = {
-    width: "370",
-    height: "350",
+    width: "300",
+    height: "300",
   };
 
   const LayarKecil = () => {
     return (
       <div className="block md:hidden">
-      <div className="flex flex-col items-center">
-        <h2 className="text-color-primary font-bold my-4">Video Trailer.</h2>
-        <div className="aspect-video border-4 border-color-accent rounded-lg">
-        <YouTube
-          videoId={youTubeId}
-          onReady={(event) => event.target.pauseVideo()}
-          opts={option}
-          onError={() => alert("video is broken, please try another!")}
-        />
+        <div className="flex flex-col items-center">
+          <h2 className="text-color-primary font-bold my-4">Video Trailer.</h2>
+          <div className="aspect-video border-4 border-color-accent rounded-lg">
+            <YouTube
+              videoId={youTubeId}
+              onReady={(event) => event.target.pauseVideo()}
+              opts={option}
+              onError={() => alert("video is broken, please try another!")}
+            />
+          </div>
         </div>
-      </div>
       </div>
     );
   };
   const LayarLebar = () => {
     return (
       <div className="hidden md:block">
-      <div className="flex flex-col items-center">
-        <h2 className="text-color-primary font-bold my-4">Video Trailer.</h2>
-        <div className="aspect-video border-4 border-color-accent rounded-lg">
-        <YouTube
-          videoId={youTubeId}
-          onReady={(event) => event.target.pauseVideo()}
-          onError={() => alert("video is broken, please try another!")}
-        />
+        <div className="flex flex-col items-center">
+          <h2 className="text-color-primary font-bold my-4">Video Trailer.</h2>
+          <div className="aspect-video border-4 border-color-accent rounded-lg">
+            <YouTube
+              videoId={youTubeId}
+              onReady={(event) => event.target.pauseVideo()}
+              onError={() => alert("video is broken, please try another!")}
+            />
+          </div>
         </div>
-      </div>
       </div>
     );
   };
@@ -46,7 +45,8 @@ const VideoPlayer = ({ youTubeId }: any) => {
     <div>
       <LayarKecil />
       <LayarLebar />
-    </div>)
+    </div>
+  );
 };
 
 export default VideoPlayer;
